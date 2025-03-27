6f3cd645080fea98c9def5c525eeff8598e55c17
Geokeke <136452962@qq.com>
2025-03-27 20:50:54 +0000

    Deploying to gh-pages from @ Geokeke/academic_pages@a541455f260f37f170d1bff0d4c9bc694d96ef26 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Please go to the publications page or the pdf version CV for a list of my publications!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/academic_pages/cv/";
